aefe854ee48c2a8bcfc568cba6853d883f712cda
Jamie Kook <kookjc6@gmail.com>
2020-11-17 17:02:15 -0700

    finished ship movement and added bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -11,10 +12,14 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        #self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
 
     def run_game(self):
@@ -23,6 +28,7 @@ class AlienInvasion:
 
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._updated_screen()
 
             # Make the most recently drawn screen visible. 
@@ -34,22 +40,57 @@ class AlienInvasion:
                 if event.type == pygame.QUIT: 
                     sys.exit()
                 elif event.type == pygame.KEYDOWN: 
-                    if event.key == pygame.K_RIGHT: 
-                        #move the ship to the right. 
-                        self.ship.moving_right = True
-                    elif event.key == pygame.K_LEFT: 
-                        self.ship.moving_left = True
+                    self._check_keydown_events(event)
                 elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT: 
-                        self.ship.moving_right = False
-                    elif event.key == pygame.K_LEFT: 
-                        self.ship.moving_left = False 
+                    self._check_keyup_events(event)
+                    
+
+    def _check_keydown_events(self, event): 
+        if event.key == pygame.K_RIGHT: 
+            #move the ship to the right. 
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT: 
+            self.ship.moving_left = True
+        elif event.key == pygame.K_UP: 
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+        elif event.key == pygame.K_q: 
+            sys.exit()
+        elif event.key == pygame.K_SPACE: 
+            self._fire_bullet()
+
+    def _check_keyup_events(self,event): 
+        if event.key == pygame.K_RIGHT: 
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT: 
+            self.ship.moving_left = False 
+        elif event.key == pygame.K_UP: 
+            self.ship.moving_up= False 
+        elif event.key == pygame.K_DOWN: 
+            self.ship.moving_down = False 
+
+    def _fire_bullet(self): 
+        if len(self.bullets) < self.settings.bullets_allowed: 
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self): 
+        self.bullets.update()
+        # Get rid of bullets that have disappeared
+        for bullet in self.bullets.copy(): 
+            if bullet.rect.bottom <= 0: 
+                self.bullets.remove(bullet)
+        print(len(self.bullets))
+
 
     def _updated_screen(self): 
         """Update images on the screen, and flip to the new screen."""
         # Redraw the screen during each pass through the loop. 
         self.screen.blit(self.settings.bg_image, (0,0))
         self.ship.blitme()
+        for bullet in self.bullets.sprites(): 
+            bullet.draw_bullet()
 
 if __name__ == '__main__': 
     #Make a game instance, and run the game. 
--- settings.py
+++ settings.py
@@ -10,3 +10,13 @@ class Settings:
 		self.screen_height = 650
 		self.bg_color = (255, 201, 247)
 		self.bg_image = pygame.image.load('images/landscape-4032192_1920.bmp')
+
+		# Ship settings
+		self.ship_speed = 1.5
+
+		#Bullet settings
+		self.bullet_speed = 1.0
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = (16, 230, 205)
+		self.bullets_allowed = 3
